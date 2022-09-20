3a726523dd384dfd233f82c7ab6ee42a7e01fcfd
montawork <hmonta.work@gmail.com>
2022-09-20 12:13:59 +0200

    install react ts & axios & react-router-dom & mui

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -63,8 +63,37 @@ const getAllUsers = async (req, res) => {
   }
 };
 
+// GET ONE USER BY ID
+const getOneUser = async (req, res) => {
+  try {
+    const user = await User.findOne({ where: { id: req.params.id } });
+    if (!user) {
+      res.status(400).json({ msg: 'User not exist' });
+    }
+    return res.json(user);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// DELETE USER
+const DestroyUser = async (req, res) => {
+  try {
+    await User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json({ msg: 'User deleted' });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 module.exports = {
   register,
   login,
   getAllUsers,
+  getOneUser,
+  DestroyUser,
 };
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -4,10 +4,14 @@ const {
   register,
   login,
   getAllUsers,
+  getOneUser,
+  DestroyUser,
 } = require('../controllers/user.controller');
 
 module.exports = (app) => {
   app.post('/api/register', register);
   app.post('/api/login', login);
   app.get('/api/users', isAuth, getAllUsers);
+  app.get('/api/users/:id', getOneUser);
+  app.delete('/api/users/:id', DestroyUser);
 };
